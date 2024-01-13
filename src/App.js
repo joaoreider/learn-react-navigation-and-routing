da76d1dc5c5b1fe05340c1717b142fa31e2e6587
Joao Paulo <joaopauloj405@gmail.com>
2024-01-13 15:02:02 -0300

    Refactor App.js and add Button component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import Button from "./Button";
-import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
+import Accordion from "./components/Accordion";
+
 function App() {
-  return (
-    <div>
-      <div>
-        <Button primary rounded className="m-2">
-          Primary
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          <GoBell />
-          secondary
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          {" "}
-          <GoCloud />
-          success{" "}
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          {" "}
-          <GoDatabase />
-          warning{" "}
-        </Button>
-      </div>
-      <div>
-        <Button danger outline rounded>
-          {" "}
-          danger{" "}
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: "jfhf",
+      label: "exemplo 1",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text  and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i",
+    },
+    {
+      id: "sfsdjkfh",
+      label: "exemplo 2",
+      content:
+        "ever since the 1500s, when an unknown printer took a galley of type",
+    },
+    {
+      id: "dfiasof",
+      label: "exemplo 3",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has bee 3 when an unknown printer took a galley of type",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,41 @@
+import Button from "../components/Button";
+import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
+function App() {
+  return (
+    <div>
+      <div>
+        <Button primary rounded className="m-2">
+          Primary
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoBell />
+          secondary
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          {" "}
+          <GoCloud />
+          success{" "}
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          {" "}
+          <GoDatabase />
+          warning{" "}
+        </Button>
+      </div>
+      <div>
+        <Button danger outline rounded>
+          {" "}
+          danger{" "}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
