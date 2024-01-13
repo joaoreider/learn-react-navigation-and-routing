d2da2dfa4c2fa68ba3a1264be2169ffabeeaba21
Joao Paulo <joaopauloj405@gmail.com>
2024-01-13 16:50:42 -0300

    Add Accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    // && operator gives the last truthy value (the div)
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => {
+            if (isExpanded) {
+              setExpandedIndex(-1);
+            } else {
+              setExpandedIndex(index);
+            }
+          }}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
+}
+
+export default Accordion;
