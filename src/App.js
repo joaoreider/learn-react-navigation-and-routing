fbb41c76220c74a2dc288bdabfe6be98ea78853f
Joao Paulo <joaopauloj405@gmail.com>
2024-01-14 11:59:54 -0300

    Add state and event handler to Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 function App() {
+  const [selection, setSelection] = useState(null);
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
   const options = [
     { label: "RED", value: "red" },
     { label: "GREEN", value: "green" },
     { label: "BLUE", value: "blue" },
   ];
 
-  return <Dropdown options={options} />;
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 
 export default App;
