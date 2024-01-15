9579675db87ab0c2c3874450de9ed2adfc38b8c0
Joao Paulo <joaopauloj405@gmail.com>
2024-01-15 09:55:02 -0300

    add NavigationProvider context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
 function App() {
-  const [selection, setSelection] = useState(null);
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-  const options = [
-    { label: "RED", value: "red" },
-    { label: "GREEN", value: "green" },
-    { label: "BLUE", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
