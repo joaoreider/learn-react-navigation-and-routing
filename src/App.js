46b7da0afca9e193157a920d0ccf54be57e55117
Joao Paulo <joaopauloj405@gmail.com>
2024-01-15 20:17:04 -0300

    Add Route component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Link from "./components/Link";
+import Route from "./components/Route";
+import AcordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
 function App() {
   return (
     <div>
       <Link to={"/accordion"}>Go to Accordion</Link>
       <Link to={"/dropdown"}>Go to DropDown</Link>
+      <div>
+        <Route path="/accordion">
+          <AcordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
