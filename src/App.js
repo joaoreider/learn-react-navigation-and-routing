01694b2c41833aa12b39970e4ff04ca51e3aa9e4
Joao Paulo <joaopauloj405@gmail.com>
2024-01-15 21:01:05 -0300

    Add Modal component and ModalPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AcordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import Sidebar from "./components/Sidebar";
+import ModalPage from "./pages/ModalPage";
 function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -16,6 +17,9 @@ function App() {
         </Route>
         <Route path="/">
           <DropdownPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
---
+++ src/pages/ModalPage.js
@@ -0,0 +1,8 @@
+import Modal from "../components/Modal";
+export default function ModalPage() {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+}
