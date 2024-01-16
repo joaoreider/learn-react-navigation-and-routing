be12ccfd4f32d57b7692ca8cee8ff081f492b43c
Joao Paulo <joaopauloj405@gmail.com>
2024-01-16 11:00:09 -0300

    Refactor Modal component and add modal functionality to ModalPage

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,8 +1,34 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 export default function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(true);
+  };
+  const handleClose = () => {
+    setShowModal(false);
+  };
+  const modal = (
+    <Modal
+      onClose={handleClose}
+      actionBar={
+        <div>
+          <Button onClick={handleClose} primary>
+            I Accept
+          </Button>
+        </div>
+      }
+    >
+      <p>important agreement for you to accept</p>
+    </Modal>
+  );
   return (
     <div>
-      <Modal />
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   );
 }
