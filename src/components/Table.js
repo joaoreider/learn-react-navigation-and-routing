0a79247984f266600298960b85a95f6f246f10c5
Joao Paulo <joaopauloj405@gmail.com>
2024-01-16 17:11:37 -0300

    Refactor Table component to accept data prop

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,3 +1,25 @@
-export default function Table() {
-  return <div>Table</div>;
+export default function Table({ data }) {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td className="p-3">{fruit.score}</td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
